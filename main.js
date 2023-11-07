6a36196a71c78e89d75d0040e421f4a8247e4734
OdetaDz <o.dziugyte@gmail.com>
2023-11-07 14:31:29 +0200

    ketvirto mygtuko funkcionalumas

--- main.js
+++ main.js
@@ -106,3 +106,39 @@ document.querySelector('#about').addEventListener('click', () => {
 
     document.querySelector('#modalui').appendChild(modalas);
 });
+
+document.querySelector('#contact').addEventListener('click', () => {
+    document.querySelector('#puslapis').classList.add('openDialog');
+    const modalas = document.createElement('dialog');
+
+    const div1 = document.createElement('div');
+    const modalName = document.createElement('h1');
+    const modalNameText = document.createTextNode('CONTACT');
+    modalName.appendChild(modalNameText);
+
+    const close = document.createElement('img');
+    close.setAttribute('src', './resources/xmark-solid.svg');
+    close.addEventListener('click', e => {
+        modalas.close();
+        modalas.remove();
+        document.querySelector('#puslapis').classList.remove('openDialog');
+    });
+
+    div1.append(modalName, close);
+
+    const div2 = document.createElement('div');
+    const modalImage = document.createElement('img');
+    modalImage.setAttribute('src', './resources/tab-4.jpg');
+
+    const modalParagraph1 = document.createElement('p');
+    const modalParagraph1Text = document.createTextNode('Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum repellat possimus, doloremque dolorum voluptatem dolore iusto quasi repudiandae tenetur. Ducimus, sapiente?');
+    modalParagraph1.appendChild(modalParagraph1Text);
+    const modalParagraph2 = document.createElement('p');
+    const modalParagraph2Text = document.createTextNode('Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum repellat possimus, doloremque dolorum voluptatem dolore iusto quasi repudiandae tenetur. Ducimus, sapiente?');
+    modalParagraph2.appendChild(modalParagraph2Text);
+
+    div2.append(modalImage, modalParagraph1, modalParagraph2);
+    modalas.append(div1, div2);
+
+    document.querySelector('#modalui').appendChild(modalas);
+});
